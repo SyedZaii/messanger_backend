18f9255eb4ab7eae4c90c929ba09ec10e4012e15
Syed Zainul <syedzainulll@gmail.com>
2025-04-23 01:53:51 +0530

    new update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:5174',
+    origin: 'http://messanger-frontend-pi.vercel.app',
     methods: ['GET', 'POST'],
     credentials: true
   }
@@ -100,5 +100,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(8000, () => {
-  console.log('Server is running on http://localhost:8000');
+  console.log('Server is running on http://messanger-frontend-pi.vercel.app');
 });
